ee25f9d909944b410065af95aecacaac3d072476
Wakhekile  Maseko <wakhekilemaseko@gmail.com>
2024-06-20 20:17:55 +0200

    update

--- Shopping Cart/index.html/script.js
+++ Shopping Cart/index.html/script.js
@@ -50,6 +50,17 @@ document.getElementById('root').innerHTML = categories.map((item)=>
 {
     var {image, item price} = item;
     return (
-        `div class=`box
+        `<div class='box'>
+          <div class='img-box'>
+            <img class='images' src=${image}></img>
+        <div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>${price}.00</h2> `+
+        "<button onclick='addtocart("+(i++)+")'>Add To Cart</button>"+
+        `</div>
+        </div>`
+           
+        
     )
-})    
+}).join('')    

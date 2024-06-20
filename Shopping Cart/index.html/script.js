65f9aa9cfc2bc56af62a96b5bcc90f0124a3cae0
Wakhekile  Maseko <wakhekilemaseko@gmail.com>
2024-06-20 20:09:31 +0200

    updte

--- Shopping Cart/index.html/script.js
+++ Shopping Cart/index.html/script.js
@@ -1,108 +1,55 @@
-let openShopping = document.querySelector('.shopping');
-let closeShopping = document.querySelector('.closeShopping');
-let list = document.querySelector('.list');
-let listProduct = document.querySelector('.listProduct');
-let body = document.querySelector('body');
-let total = document.querySelector('.total');
-let quantity = document.querySelector('.quantity');
-
-openShopping.addEventListener('click', ()=>{
-    body.classList.add('active');
-})
-closeShopping.addEventListener('click', ()=>{
-    body.classList.remove('active');
-})
-
-let products = [
+const product = [
     {
         id: 1,
-        name: 'Air Force 1',
-        image: 'imaheAF1.jpeg',
-        price: 1499.99
+        image: 'image/imageAF.jpg',
+        title: 'Air Force 1',
+        price: 50.00
     },
     {
         id: 2,
-        name: 'Air Jordan',
-        image: 'imageAJ.jpg',
-        price: 2499.99
+        Image: 'image/imageAJ.jpg',
+        title: 'Air Jordan',
+        price: 40.00
+
     },
     {
         id: 3,
-        name: 'Airmax',
-        image: 'imageAM.jpg',
-        price: 2799.99
+        Image: 'image/imageAM',
+        title: 'Air Max',
+        price: 70.00
+
     },
     {
         id: 4,
-        name: 'Blaze',
-        image: 'imageBL.jpg',
-        price: 1899.99
+        Image: 'image/imageBL',
+        title: 'Blaze',
+        price: 30.00
+
     },
     {
         id: 5,
-        name: 'High Dunk',
-        image: 'imageHD.jpg',
-        price: 2399.99
+        Image: 'image/imageHD',
+        title: 'High Dunk',
+        price: 80.00
+
     },
     {
         id: 6,
-        name: 'Low Dunk',
-        image: 'imageLD.jpg',
-        price: 2699.99
-    }
+        Image: 'image/imageLD.jpg',
+        title: 'Low Dunk',
+        price: 90.00
+
+    },
+    
+
 ];
-let listCards  = [];
-function initApp(){
-    products.forEach((value, key) =>{
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('product');
-        newDiv.innerHTML = `
-            <img src="image/${value.image}">
-            <div class="title">${value.name}</div>
-            <div class="price">${value.price.toLocaleString()}</div>
-            <button onclick="addToCard(${key})">Add To Cart</button>`;
-        list.appendChild(newDiv);
-    })
-}
-initApp();
-function addToCard(key){
-    if(listCards[key] == null){
-        // copy product form list to list card
-        listCart[key] = JSON.parse(JSON.stringify(products[key]));
-        listCart[key].quantity = 1;
-    }
-    reloadCard();
-}
-function reloadCard(){
-    listCart.innerHTML = '';
-    let count = 0;
-    let totalPrice = 0;
-    listCarts.forEach((value, key)=>{
-        totalPrice = totalPrice + value.price;
-        count = count + value.quantity;
-        if(value != null){
-            let newDiv = document.createElement('li');
-            newDiv.innerHTML = `
-                <div><img src="image/${value.image}"/></div>
-                <div>${value.name}</div>
-                <div>${value.price.toLocaleString()}</div>
-                <div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-                    <div class="count">${value.quantity}</div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
-                </div>`;
-                listCart.appendChild(newDiv);
-        }
-    })
-    total.innerText = totalPrice.toLocaleString();
-    quantity.innerText = count;
-}
-function changeQuantity(key, quantity){
-    if(quantity == 0){
-        delete listProduct[key];
-    }else{
-        listCart[key].quantity = quantity;
-        listCart[key].price = quantity * products[key].price;
-    }
-    reloadCart();
-}
+const categories = [...new Set(product.map((item)=>
+    {return}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, item price} = item;
+    return (
+        `div class=`box
+    )
+})    
